2b2316d9308048941a4e6075338ebd7335064ad9
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-02 09:49:33 +0500

    refactor: reorganize layout and improve styling in RootFooter component

--- src/app/_components/root-footer.tsx
+++ src/app/_components/root-footer.tsx
@@ -53,150 +53,162 @@ export function RootFooter() {
   ];
 
   return (
-    <footer className={cn("py-16 px-24 sticky top-0 z-10 bg-muted")}>
-      <div className={cn("max-w-screen-xl mx-auto flex justify-between")}>
-        <div className={cn("flex flex-col gap-6 items-center")}>
-          <Link href="/">
-            <img
-              src={assets.logo.asset.src}
-              alt={assets.logo.alt}
-              className={cn("h-28 w-auto")}
-            />
-          </Link>
-          <p
-            className={cn(
-              "max-w-72 text-foreground/65 font-medium text-center",
-            )}
-          >
-            Connect with trusted vendors and contractors to reduce waste, cut
-            costs and contribute to a greener future.
-          </p>
-          <div className={cn("flex gap-2")}>
-            <Input placeholder="john@domain.com" className={cn("w-full")} />
+    <footer>
+      <div className={cn("py-16 px-24 sticky top-0 z-10 bg-muted")}>
+        <div className={cn("max-w-screen-xl mx-auto flex justify-between")}>
+          <div className={cn("flex flex-col gap-6 items-center")}>
+            <Link href="/">
+              <img
+                src={assets.logo.asset.src}
+                alt={assets.logo.alt}
+                className={cn("h-28 w-auto")}
+              />
+            </Link>
+            <p
+              className={cn(
+                "max-w-72 text-foreground/65 font-medium text-center",
+              )}
+            >
+              Connect with trusted vendors and contractors to reduce waste, cut
+              costs and contribute to a greener future.
+            </p>
+            <div className={cn("flex gap-2")}>
+              <Input placeholder="john@domain.com" className={cn("w-full")} />
 
-            <Button variant="default" size="sm">
-              Subscribe
-            </Button>
+              <Button variant="default" size="sm">
+                Subscribe
+              </Button>
+            </div>
           </div>
+          <ul className={cn("space-y-3")}>
+            <h3
+              className={cn("mb-6 text-foreground text-xl font-medium", domine)}
+            >
+              Our Listings
+            </h3>
+            {listingLinks.map((route) => (
+              <li key={route.label}>
+                <Link
+                  href={route.url()}
+                  className={cn(
+                    "text-foreground/65 hover:text-primary transition-all duration-300",
+                  )}
+                >
+                  {route.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className={cn("space-y-3")}>
+            <h3
+              className={cn("mb-6 text-foreground text-xl font-medium", domine)}
+            >
+              Quick Links
+            </h3>
+            {navLinks.map((route) => (
+              <li key={route.label}>
+                <Link
+                  href={route.url()}
+                  className={cn(
+                    "text-foreground/65 hover:text-primary transition-all duration-300",
+                  )}
+                >
+                  {route.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className={cn("space-y-6")}>
+            <h3 className={cn("text-foreground text-xl font-medium", domine)}>
+              Contact Us
+            </h3>
+            <li>
+              <Link
+                href="tel:801-998-2063"
+                className={cn(
+                  "flex gap-4 items-center text-foreground/65 hover:text-primary transition-all duration-300",
+                )}
+              >
+                <PhoneIcon className={cn("size-5 text-primary")} />
+                <span>801-998-2063</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="mailto:help@ecobuilt.com"
+                className={cn(
+                  "flex gap-4 items-center text-foreground/65 hover:text-primary transition-all duration-300",
+                )}
+              >
+                <MailIcon className={cn("size-5 text-primary")} />
+                <span>help@ecobuilt.com</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href=""
+                className={cn(
+                  "flex gap-4 items-center text-foreground/65 hover:text-primary transition-all duration-300",
+                )}
+              >
+                <MapPinIcon className={cn("size-6 text-primary")} />
+                <span className={cn("max-w-48")}>
+                  9418 S Feulner Road, West Jordan, Utah 84081
+                </span>
+              </Link>
+            </li>
+            <li>
+              <ul className={cn("flex gap-4 ml-1 mt-10")}>
+                <li>
+                  <Link href="">
+                    <SiFacebook
+                      className={cn(
+                        "size-6 text-foreground/65 hover:text-primary transition-all duration-300",
+                      )}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="">
+                    <SiInstagram
+                      className={cn(
+                        "size-6 text-foreground/65 hover:text-primary transition-all duration-300",
+                      )}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="">
+                    <SiYoutube
+                      className={cn(
+                        "size-6 text-foreground/65 hover:text-primary transition-all duration-300",
+                      )}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link href="">
+                    <SiX
+                      className={cn(
+                        "size-6 text-foreground/65 hover:text-primary transition-all duration-300",
+                      )}
+                    />
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
-        <ul className={cn("space-y-3")}>
-          <h3
-            className={cn("mb-6 text-foreground text-xl font-medium", domine)}
-          >
-            Our Listings
-          </h3>
-          {listingLinks.map((route) => (
-            <li key={route.label}>
-              <Link
-                href={route.url()}
-                className={cn(
-                  "text-foreground/65 hover:text-primary transition-all duration-300",
-                )}
-              >
-                {route.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul className={cn("space-y-3")}>
-          <h3
-            className={cn("mb-6 text-foreground text-xl font-medium", domine)}
-          >
-            Quick Links
-          </h3>
-          {navLinks.map((route) => (
-            <li key={route.label}>
-              <Link
-                href={route.url()}
-                className={cn(
-                  "text-foreground/65 hover:text-primary transition-all duration-300",
-                )}
-              >
-                {route.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul className={cn("space-y-6")}>
-          <h3 className={cn("text-foreground text-xl font-medium", domine)}>
-            Contact Us
-          </h3>
-          <li>
-            <Link
-              href="tel:801-998-2063"
-              className={cn(
-                "flex gap-4 items-center text-foreground/65 hover:text-primary transition-all duration-300",
-              )}
-            >
-              <PhoneIcon className={cn("size-5 text-primary")} />
-              <span>801-998-2063</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="mailto:help@ecobuilt.com"
-              className={cn(
-                "flex gap-4 items-center text-foreground/65 hover:text-primary transition-all duration-300",
-              )}
-            >
-              <MailIcon className={cn("size-5 text-primary")} />
-              <span>help@ecobuilt.com</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href=""
-              className={cn(
-                "flex gap-4 items-center text-foreground/65 hover:text-primary transition-all duration-300",
-              )}
-            >
-              <MapPinIcon className={cn("size-6 text-primary")} />
-              <span className={cn("max-w-48")}>
-                9418 S Feulner Road, West Jordan, Utah 84081
-              </span>
-            </Link>
-          </li>
-          <li>
-            <ul className={cn("flex gap-4 ml-1 mt-10")}>
-              <li>
-                <Link href="">
-                  <SiFacebook
-                    className={cn(
-                      "size-6 text-foreground/65 hover:text-primary transition-all duration-300",
-                    )}
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link href="">
-                  <SiInstagram
-                    className={cn(
-                      "size-6 text-foreground/65 hover:text-primary transition-all duration-300",
-                    )}
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link href="">
-                  <SiYoutube
-                    className={cn(
-                      "size-6 text-foreground/65 hover:text-primary transition-all duration-300",
-                    )}
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link href="">
-                  <SiX
-                    className={cn(
-                      "size-6 text-foreground/65 hover:text-primary transition-all duration-300",
-                    )}
-                  />
-                </Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
+      </div>
+      <div
+        className={cn(
+          "px-8 py-4 bg-gradient-to-r from-sky-600 to-sky-500 text-primary-foreground",
+        )}
+      >
+        <p className={cn("text-center text-sm")}>
+          © 2021 Ecobuilt. All rights reserved. Privacy Policy | Terms of
+          Service
+        </p>
       </div>
     </footer>
   );

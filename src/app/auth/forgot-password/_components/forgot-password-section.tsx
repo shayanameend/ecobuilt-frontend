87c0e3d30bb4e62a650d445c3f6a192f4eaa5ab5
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-06 08:35:42 +0500

    feat: add FormDescription to ForgotPasswordSection for improved user guidance

--- src/app/auth/forgot-password/_components/forgot-password-section.tsx
+++ src/app/auth/forgot-password/_components/forgot-password-section.tsx
@@ -14,6 +14,7 @@ import { Button } from "~/components/ui/button";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -108,6 +109,14 @@ export function ForgotPasswordSection() {
                     <Input placeholder="john@domain.com" {...field} />
                   </FormControl>
                   <FormMessage />
+                  <FormDescription className={cn("text-right")}>
+                    <Link
+                      href={authRoutes.forgotPassword.url()}
+                      className={cn("underline underline-offset-4")}
+                    >
+                      Forgot password?
+                    </Link>
+                  </FormDescription>
                 </FormItem>
               )}
             />
